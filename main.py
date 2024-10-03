96ebcb1b36383d565da2fda035962839cd88355f
Jakub Wojtkowski <jakubwojtkowski@masnyBook.local>
2024-10-03 22:50:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import webbrowser
+import time
+
+
+chrome = webbrowser.Chrome
+
+
+def main():
+
+    print("You're getting rick'n'rolled :DD")
+
+    time.sleep(5)
+    webbrowser.open(url="https://youtu.be/dQw4w9WgXcQ?si=SFqBL8wzeKIL_Gu3")
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
